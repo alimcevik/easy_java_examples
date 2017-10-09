99f2155a6d87e396eedd607e63f816877a977192
Ali Murat Çevik <murat.cevik@bil.omu.edu.tr>
2017-10-09 19:14:38 +0300

    Add files via upload

---
+++ carpimtablosu.java
@@ -0,0 +1,14 @@
+public class carpimtablosu {
+    
+  public static void main(String[] args) {
+    
+    for ( int x =1 ; x<=10; x++){
+      for (int y = 1; y<=10; y++){
+      System.out.println(x + "*" +y +"=" + x *y+ "\t");
+    }                 
+		System.out.println("");
+ 			}
+     
+
+    }
+}
