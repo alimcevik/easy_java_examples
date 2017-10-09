7db9ef271e0371565153fe2de0d688a2587ac6fb
Ali Murat Çevik <murat.cevik@bil.omu.edu.tr>
2017-10-09 19:19:25 +0300

    Update carpimtablosu.java

--- carpimtablosu.java
+++ carpimtablosu.java
@@ -1,14 +1,13 @@
 public class carpimtablosu {
-    
   public static void main(String[] args) {
     
-    for ( int x =1 ; x<=10; x++){
-      for (int y = 1; y<=10; y++){
+  for ( int x =1 ; x<=10; x++){
+    for (int y = 1; y<=10; y++){
       System.out.println(x + "*" +y +"=" + x *y+ "\t");
-    }                 
-		System.out.println("");
- 			}
+  }                 
+System.out.println("");
+    }
      
 
-    }
+  }
 }
