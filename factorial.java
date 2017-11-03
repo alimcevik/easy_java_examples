35a68ca415f7dec0baeb17e8fa8c6f3d8ab84e7c
Ali Murat Çevik <murat.cevik@bil.omu.edu.tr>
2017-11-03 20:52:45 +0300

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class factorial {
+  public static void main(String[] args) {
+   Scanner input = new Scanner (System.in);
+
+   int num,factorial = 1;
+   System.out.println("Faktöriyeli alınacak sayıyı girin: ");
+   num = input.nextInt();
+        
+   for (int counter =2 ;counter <=num;counter++){
+     factorial = factorial * counter;
+   }
+
+   System.out.println(num + "! = " + factorial);   
+  }
+}
